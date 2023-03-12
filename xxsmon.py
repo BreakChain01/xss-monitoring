34a3beb4901df6b518e53ac30830c57ecfb0d420
BreakChain01 <122126163+BreakChain01@users.noreply.github.com>
2023-03-12 18:33:45 +0300

    Create xxsmon.py

---
+++ xxsmon.py
@@ -0,0 +1,23 @@
+        for (let i = 0; i < comments.length; ++i)
+        {
+            comment = comments[i];
+            let commentSection = document.createElement("section");
+            commentSection.setAttribute("class", "comment");
+
+            let firstPElement = document.createElement("p");
+
+            let avatarImgElement = document.createElement("img");
+            avatarImg Element.setAttribute("class", "avatar");
+            avatarImgElement.setAttribute("src", comment.avatar ? comment.avatar : "/resources/images/avatarDefault.svg");
+
+            if (comment.author) {
+                if (comment.website) {
+                    let websiteElement = document.createElement("a");
+                    websiteElement.setAttribute("id", "author");
+                    websiteElement.setAttribute("href", comment.website);
+                    firstPElement.appendChild(websiteElement)
+                }
+
+                let newInnerHtml = firstPElement.innerHTML + janitor.clean(comment.author)
+                firstPElement.innerHTML = newInnerHtml
+            }
